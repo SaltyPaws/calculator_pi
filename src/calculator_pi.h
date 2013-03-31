2c92ec1c42e3180f95a6e369c102a56a3b905161
SaltyPaws <walbertr@gmx.net>
2013-03-31 15:50:07 +0200

    split gui for windows and linux, as wxformbuilder does not give nice look to both with one gui file

--- src/calculator_pi.h
+++ src/calculator_pi.h
@@ -41,7 +41,7 @@
 #include "calculatorgui_impl.h"
 
 #define     PLUGIN_VERSION_MAJOR    1
-#define     PLUGIN_VERSION_MINOR    2
+#define     PLUGIN_VERSION_MINOR    3
 
 #define     MY_API_VERSION_MAJOR    1
 #define     MY_API_VERSION_MINOR    8
